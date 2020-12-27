3a03f5a0396a602c360eb19dbbe822cb633f6b50
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-12-26 21:07:33 -0500

    Frontend Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/* Components */
+import { HeaderComponent } from './shared/header/header.component';
+
+const routes: Routes = [
+  { path: 'landingPage', component: HeaderComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+  //{ path: '**', component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
